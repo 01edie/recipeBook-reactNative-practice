1a3013208f872775de402a5681eee035a8f74f46
BISWAJIT BOSE <97077410+01edie@users.noreply.github.com>
2023-02-26 20:01:19 +0530

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import favoritesSlice from './favoritesSlice'
 
-favoritesSlice
 export const store = configureStore({
     reducer: favoritesSlice,
     
